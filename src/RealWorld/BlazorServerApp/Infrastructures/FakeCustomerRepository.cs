9f9124db48269410faed0abfffac7dad49869b0d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-04-14 14:09:30 +0200

    Refaktoring: Rejestracja listy klientów

--- src/RealWorld/BlazorServerApp/Infrastructures/FakeCustomerRepository.cs
+++ src/RealWorld/BlazorServerApp/Infrastructures/FakeCustomerRepository.cs
@@ -7,14 +7,8 @@ public class FakeCustomerRepository : ICustomerRepository
 {
     private readonly IDictionary<int, Customer> _customers;
 
-    public FakeCustomerRepository()
-    {
-        var customers = new List<Customer>();
-        customers.Add(new Customer { Id = 1, Name = "Customer #1", Email = "a@domain.com" });
-        customers.Add(new Customer { Id = 2, Name = "Customer #2", Email = "b@domain.com" });
-        customers.Add(new Customer { Id = 3, Name = "Customer #3", Email = "c@domain.com" });
-        customers.Add(new Customer { Id = 4, Name = "Customer #4", Email = "d@domain.com" });
-        customers.Add(new Customer { Id = 5, Name = "Customer #5", Email = "e@domain.com" });
+    public FakeCustomerRepository(List<Customer> customers)
+    {        
 
         _customers = customers.ToDictionary(c => c.Id);
     }
--- src/RealWorld/BlazorServerApp/Program.cs
+++ src/RealWorld/BlazorServerApp/Program.cs
@@ -1,10 +1,22 @@
 using BlazorServerApp.Components;
+using BlazorServerApp.Domain;
 using BlazorServerApp.Domain.Abstractions;
 using BlazorServerApp.Infrastructures;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<ICustomerRepository, FakeCustomerRepository>();
+builder.Services.AddSingleton<List<Customer>>(sp =>
+{
+    var customers = new List<Customer>();
+    customers.Add(new Customer { Id = 1, Name = "Customer #1", Email = "a@domain.com" });
+    customers.Add(new Customer { Id = 2, Name = "Customer #2", Email = "b@domain.com" });
+    customers.Add(new Customer { Id = 3, Name = "Customer #3", Email = "c@domain.com" });
+    customers.Add(new Customer { Id = 4, Name = "Customer #4", Email = "d@domain.com" });
+    customers.Add(new Customer { Id = 5, Name = "Customer #5", Email = "e@domain.com" });
+
+    return customers;
+});
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
