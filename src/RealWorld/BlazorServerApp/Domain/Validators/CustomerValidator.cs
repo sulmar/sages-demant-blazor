f921ac8de332fdc90ca35434caa6ab0f7840b4f7
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-04-15 16:46:16 +0200

    Dodanie walidacji za pomocą FluentValidations

---
+++ src/RealWorld/BlazorServerApp/Domain/Validators/CustomerValidator.cs
@@ -0,0 +1,27 @@
+﻿using BlazorServerApp.Domain.Abstractions;
+using FluentValidation;
+
+namespace BlazorServerApp.Domain.Validators;
+
+public class CustomerValidator : AbstractValidator<Customer>
+{
+    public CustomerValidator()
+    {
+        RuleFor(c => c.Name)
+            .NotEmpty()
+            .Length(3, 10);
+
+        RuleFor(c => c.Email)
+            .NotEmpty()
+            .EmailAddress();
+
+        RuleFor(c => c.HashedPassword)
+            .NotEmpty()
+            .WithMessage("Password is required.");
+
+        RuleFor(c => c.ConfirmedPassword)
+            .Equal(c => c.HashedPassword)
+            .WithMessage("The password and confirmation password do not match.");
+
+    }
+}
