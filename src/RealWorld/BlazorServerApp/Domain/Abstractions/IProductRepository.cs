9009bdf7c7eb073845eee43cc1053d3dd4c16c0a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2025-04-15 11:07:22 +0200

    Dodanie panelu wyszukiwania do produktów

--- src/RealWorld/BlazorServerApp/Domain/Abstractions/IProductRepository.cs
+++ src/RealWorld/BlazorServerApp/Domain/Abstractions/IProductRepository.cs
@@ -3,6 +3,7 @@
 public interface IProductRepository : IEntityRepository<Product>
 {
     Task<IEnumerable<Product>> GetByColorAsync(string color);
+    Task<IEnumerable<Product>> GetByTextAsync(string text); 
 }
 
 
--- src/RealWorld/BlazorServerApp/Infrastructures/FakeProductRepository.cs
+++ src/RealWorld/BlazorServerApp/Infrastructures/FakeProductRepository.cs
@@ -13,4 +13,12 @@ public class FakeProductRepository : FakeEntityRepository<Product>, IProductRepo
     {
         throw new NotImplementedException();
     }
+
+    public async Task<IEnumerable<Product>> GetByTextAsync(string text)
+    {
+        await Task.Delay(500);
+        return _entities.Select(p => p.Value)
+            .Where(c => c.Name.Contains(text, StringComparison.OrdinalIgnoreCase) 
+            || c.Description.Contains(text, StringComparison.OrdinalIgnoreCase) || c.Price.ToString().Contains(text));
+    }
 }
